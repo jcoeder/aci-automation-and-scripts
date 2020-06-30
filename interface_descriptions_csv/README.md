d7cdd40ba5443e8a207c7901a21c4275c7bef5dd
Justin Oeder <jcoeder@gmail.com>
2020-06-30 12:48:13 -0400

    Add files via upload

---
+++ interface_descriptions_csv/README.md
@@ -0,0 +1,55 @@
+# interface_descriptions
+Script to set Physical Interface Configuration Description in Cisco ACI
+
+### Install Python3.7 CentOS7
+Make sure Python3.7 is installed on your system in one way or another.  Here is how you do it in CentOS7
+```
+sudo yum install gcc openssl-devel bzip2-devel libffi libffi-devel sqlite sqlite-devel -y
+sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+sudo tar xzf Python-3.7.3.tgz
+cd Python-3.7.3
+sudo ./configure --enable-optimizations
+sudo make altinstall
+pip3.7 install virtualenv --user
+pip3.7 install --upgrade pip
+pip3.7 install --upgrade wheel
+```
+
+### Clone this repository
+```git clone https://github.com/jcoeder/aci-automation-and-scripts.git```
+
+### Navigate to this directory
+```cd /aci-automation-and-scripts/interface_descriptions```
+
+### Create your virtual environment to run Python
+```
+python3.7 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+### Edit the interface_description.py with the IP address of the APIC
+You can use any editor you are familiar with.  I am using VIM.
+```
+vi interface_description.py 
+```
+Update the IP address in this section.  Do not change anything else in the file.
+```
+### Update this IP Address
+### Update this IP Address
+apic = '10.5.9.11'
+### Update this IP Address
+### Update this IP Address
+```
+
+### Run the script
+```python interface_description.py```
+
+### Output
+```
+(venv) $ python interface_description.py 
+Password: 
+Node ID (Ex: 101): 102
+Interface ID (Ex: "1/1"): 1/44
+Interface Description: Testing1234
+<Response [200]>
---
+++ interface_descriptions_csv/interface_description_csv.py
@@ -0,0 +1,67 @@
+import requests
+import json
+import time
+import getpass
+import urllib3
+import re
+import csv
+
+username = 'admin'
+#password = 'PASSWORD'
+password = getpass.getpass()
+
+### Update this IP Address
+### Update this IP Address
+apic = '10.5.9.11'
+### Update this IP Address
+### Update this IP Address
+
+# Disable SSL Warnings
+urllib3.disable_warnings()
+
+
+def login():
+    '''
+    Log into APIC and return session
+    '''
+    url = 'https://' + apic + '/api/aaaLogin.json'
+    payload = {'aaaUser':{'attributes':{'name':username,'pwd':password}}}
+    session = requests.Session()
+    response = session.post(url, json=payload, verify=False)
+    return session
+
+
+def interface_format(interface_id):
+    '''
+    Format Interface ID as needed
+    '''
+    pattern = re.compile('^\d+\/\d+$')
+    match = pattern.match(interface_id)
+    if not match:
+        print("Invalid Interface Format")
+        quit()
+    else:
+        pass
+    interface_split = []
+    interface_split.append(interface_id.split('/'))
+    interface_underscore = str(interface_split[0][0]) + '_' + str(interface_split[0][1])
+    return interface_underscore#
+
+def set_interface_description(session, apic, node_id, interface_id, interface_description):
+    '''
+    Set interface description
+    '''
+    interface_underscore = interface_format(interface_id)
+    url = 'https://' + apic + '/api/node/mo/uni/infra/hpaths-' + node_id + '_eth' + interface_underscore + '.json'
+    json = {"infraHPathS":{"attributes":{"rn":"hpaths-" + str(node_id) + "_eth" + interface_underscore + "","dn":"uni/infra/hpaths-" + str(node_id) + "_eth" + interface_underscore + "","descr":"" + interface_description + "","name":"" + str(node_id) + "_eth" + interface_underscore + ""},"children":[{"infraRsHPathAtt":{"attributes":{"dn":"uni/infra/hpaths-" + str(node_id) + "_eth" + interface_underscore + "/rsHPathAtt-[topology/pod-1/paths-" + node_id + "/pathep-[eth" + interface_id + "]]","tDn":"topology/pod-1/paths-" + node_id + "/pathep-[eth" + interface_id + "]"}}}]}}
+    response = session.post(url, json=json, verify=False)
+    print(response)
+
+
+session = login()
+
+with open('interfaces.csv') as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader)
+    for row in csv_reader:
+        set_interface_description(session, apic=apic, node_id=row[0], interface_id=row[1], interface_description=row[2])
